463f07fee8eb707d1718939957dc595c8b19fc2b
KiqECS <kaiquehelper2019@gmail.com>
2021-03-01 18:05:17 -0300

    Comentários

---
+++ comment.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+            # Comment de uma linha
+            // Também comment de uma linha
+            /*
+                Comment
+                de 
+                várias
+                linhas
+            */
+        ?>
+    </body>
+</html>
