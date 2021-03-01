d2cf27d2d27d97a7fc2842167bf236f791fed46c
KiqECS <kaiquehelper2019@gmail.com>
2021-03-01 18:05:05 -0300

    Duas formas de mostrar na tela

---
+++ echo_print.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+            echo "Com echo<br/>";
+            print "Com print";
+        ?>
+    </body>
+</html>
