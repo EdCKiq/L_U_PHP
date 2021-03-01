9fe094e922bb9f7c62c39774dd27c336d93051ae
KiqECS <kaiquehelper2019@gmail.com>
2021-02-28 23:26:25 -0300

    No começo

---
+++ inicio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+<body>
+    <?php  
+        echo "Iniciando aqui"
+    ?>
+    <br/>
+    <?= "So Impressão" ?>
+</body>
+</html>
